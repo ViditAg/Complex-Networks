d2a55232b57efe2c176002076647b8ebb6e99c60
Vidit Agrawal <43018189+ViditAg@users.noreply.github.com>
2018-12-12 08:30:14 -0600

    Add files via upload

---
+++ infection spread-time series.c
@@ -0,0 +1,141 @@
+
+//Program for SIRS cycle on 2-D grid and see the infection propogation
+
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+int main(void)
+{
+int i,j,k,m,n;
+int t,tim;
+tim= 200;
+m = 40; n = 40;
+int tau[m][n];
+float initial_frac,i_f;
+float total_infect, total_s, total_r,avg_total_infect,avg_total_s,avg_total_r;
+int inf_nb[m][n];
+int tau_O,tau_i;
+int site;
+tau_O = 13;
+tau_i = 9;
+initial_frac = 0.5;                                                             // initial fraction of suseptible
+int n_rand, m_rand;  
+float neta,neta_rand;                                                           
+srand(time(0));
+FILE *fp;  
+fp = fopen("infect_spread40x40_time-series.txt","wt");
+neta = 0.0;
+
+
+for(i=0;i<m;i++)
+    {
+     for(j=0;j<n;j++)
+        {
+         //m_rand = rand() % (m+1);
+         //n_rand = rand() % (n+1);
+         i_f = (float)rand()/RAND_MAX;                                                       
+           // if(i== m_rand && j== n_rand)
+              if(i== (m/2) && j== (n/2))
+              tau[i][j] = 1;
+           // else if(i==(m/2) && j==57)
+             // tau[i][j] = 1;
+            else
+              if(i_f<initial_frac)
+                 tau[i][j] = 0;
+              else   
+               {
+                //neta_rand = (float)rand()/RAND_MAX;   
+                //if(neta_rand<neta)
+                 //{
+                 // do
+                  // { 
+                   // tau[i][j] = rand() % (tau_O + 1);
+                  // }while(tau[i][j]<=tau_i);
+                  // }
+                //else
+                 tau[i][j] = tau_i + 1;
+               }
+        }
+    }
+for(t=1;t<=tim;t++)
+{
+ avg_total_infect = 0.0;
+ avg_total_s = 0.0;
+ avg_total_r = 0.0;
+ total_infect = 0.0;
+ total_s = 0.0;
+ total_r = 0.0;
+ for(i=0;i<m;i++)
+    {                                                                                 // loop to check infected neigbors
+     for(j=0;j<n;j++)
+     {
+      inf_nb[i][j] = 0;      
+      if(i!=(m-1)) 
+        if(tau[i+1][j]>0 && tau[i+1][j]<=tau_i)
+               inf_nb[i][j]++;
+      if(i!=0)
+        if(tau[i-1][j]>0 && tau[i-1][j]<=tau_i)
+               inf_nb[i][j]++;       
+      if(j!=(n-1))
+        if(tau[i][j+1]>0 && tau[i][j+1]<=tau_i)
+               inf_nb[i][j]++;
+      if(j!=0) 
+        if(tau[i][j-1]>0 && tau[i][j-1]<=tau_i) 
+               inf_nb[i][j]++;
+      }
+    } 
+for(i=0;i<m;i++)
+    {
+     for(j=0;j<n;j++)                                                                    // disease cycle    
+     {
+     // if(i==(m/2) && j==(n/2)/*n/4*/)
+       //  tau[i][j] = 1;
+      //else if(i==(m/2) && j==(3*n/4))
+        // tau[i][j] = 1;
+     // else
+      // {
+        if(tau[i][j]==tau_O)
+         tau[i][j] = 0;
+        else if(tau[i][j]==0)
+         {
+          if(inf_nb[i][j]!=0)                                                            // depending on infected neighbors S to R
+             tau[i][j] = 1;
+         }   
+        else
+         tau[i][j] = tau[i][j] + 1;
+     //   }
+     }    
+    }                                                                                // time loop ends here
+for(i=0;i<m;i++)
+    {
+     for(j=0;j<n;j++)
+        {                                                         
+         if(tau[i][j]==0)
+         {
+          site = 0;
+          total_s++;
+         }
+         if(tau[i][j]>0 && tau[i][j]<=tau_i)
+         {
+          site = 1;
+          total_infect++;
+         }
+         if(tau[i][j]>tau_i)
+         {
+          site = 2;
+          total_r++;
+         }
+        }
+    }   
+avg_total_infect += total_infect/(n*m);
+avg_total_s += total_s/(n*m);
+avg_total_r += total_r/(n*m);
+printf("%d %f %f %f\n",t,avg_total_infect,avg_total_s,avg_total_r);
+fprintf(fp,"%d %f %f %f\n",t,avg_total_infect,avg_total_s,avg_total_r);
+}
+fclose(fp);
+printf("done");
+getch();
+return 0;
+}
